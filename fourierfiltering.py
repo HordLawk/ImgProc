448ede436026e67db248ae97d005272980071f1d
HordLawk <pedrofalcaorocha@gmail.com>
2022-05-11 18:03:30 -0300

    fix(fourier): size prop instead of calculating it

--- fourierfiltering.py
+++ fourierfiltering.py
@@ -7,12 +7,12 @@ def normalizeRange(original: np.ndarray, max: int, pixelType: type) -> np.ndarra
     return ((original - np.min(original)) / (np.max(original) - np.min(original)) * max).astype(pixelType)
 
 # Calculates the RMSE of the comparison between two images
-def RMSE(ref: np.ndarray, result: np.ndarray) -> float:
+def calcRMSE(ref: np.ndarray, result: np.ndarray) -> float:
     sums = 0
     for i, row in enumerate(ref):
         for j, pixel in enumerate(row):
-            sums += (pixel - float(result[i][j])) ** 2
-    return math.sqrt(sums / (len(ref) * len(ref[0])))
+            sums += (pixel - result[i][j]) ** 2
+    return math.sqrt(sums / ref.size)
 
 # Loading input images
 filenameI = str(input()).rstrip()
@@ -22,15 +22,15 @@ fltr = imageio.imread(filenameM)
 filenameG = str(input()).rstrip()
 reference = imageio.imread(filenameG)
 
-# Generating the Fourier Spectrum of the input image
+# Generating the Fourier Spectrum of the original image
 spectrum = np.fft.fft2(img)
 
-# Shifts the lower frequencies to the middle of the spectrum
-# Applies the filter to the spectrum
-# Reverts the shifting to return to the original arrangement
+# Shifting the lower frequencies to the middle of the spectrum
+# Applying the filter to the spectrum
+# Reverting the shifting to return to the original arrangement
 filteredSpectrum = np.fft.ifftshift(np.fft.fftshift(spectrum) * normalizeRange(fltr, 1, np.uint8))
 
-# Generates the filtered image back into the space domain
+# Generating the filtered image back into the space domain
 filteredImage = np.real(np.fft.ifft2(filteredSpectrum))
 
-print(RMSE(reference, normalizeRange(filteredImage, 255, np.uint8)))
+print(calcRMSE(reference, normalizeRange(filteredImage, 255, np.uint8)))
